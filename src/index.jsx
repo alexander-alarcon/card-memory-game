d33c9bee7747e6918a6b29984a0b7e350133b2a8
Alexander Alarcon <walarcon14@gmail.com>
2019-07-09 10:24:25 -0500

    Modify Folder Structure

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './App';
+import App from './Containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
