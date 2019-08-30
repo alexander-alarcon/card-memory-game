3fbdacdf38870ed3db5ca4b850e8f92856166074
Alexander Alarcon <walarcon14@gmail.com>
2019-08-30 11:15:48 -0500

    Updates

---
+++ precache-manifest.20e5c3b7e7444b82bfa1aacbb465217b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660062da36c4fbc960d9eb8867c747dc",
+    "url": "/card-memory-game/index.html"
+  },
+  {
+    "revision": "c5ec23c3e89934d5f824",
+    "url": "/card-memory-game/static/css/main.1f1e371d.chunk.css"
+  },
+  {
+    "revision": "f4e343a752fbb0d7a9c2",
+    "url": "/card-memory-game/static/js/2.904b307d.chunk.js"
+  },
+  {
+    "revision": "c5ec23c3e89934d5f824",
+    "url": "/card-memory-game/static/js/main.889e0cf0.chunk.js"
+  },
+  {
+    "revision": "e3d6ccd2567876f8cfa3",
+    "url": "/card-memory-game/static/js/runtime~main.1f5b3080.js"
+  }
+]);
